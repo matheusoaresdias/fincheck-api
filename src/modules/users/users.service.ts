e41212fd9a74f5f147f379f1862228cad771833f
Matheus Soares <matheus.soares.msd@gmail.com>
2025-01-03 23:07:51 -0300

    feat: user repository, hashed password and api architecture (folders arch and global pipe)

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,57 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositores';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const createdUser = await this.usersRepository.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salary', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Other', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Home', icon: 'home', type: 'EXPENSE' },
+              { name: 'Food', icon: 'food', type: 'EXPENSE' },
+              { name: 'Education', icon: 'education', type: 'EXPENSE' },
+              { name: 'Fun', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Grocery', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Clothes', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transport', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Travel', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Other', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
+
+    return {
+      name: createdUser.name,
+      email: createdUser.email,
+    };
+  }
+}
